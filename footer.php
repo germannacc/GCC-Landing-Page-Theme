d56e145d3083418f114e77ad2239562eb5e35736
Matt Hummel <marketing@users.noreply.github.com>
2019-10-10 11:51:44 -0400

    Update
    Improved footer view for mobile.  New classes added.

--- footer.php
+++ footer.php
@@ -9,22 +9,23 @@
 * @package gcc-starterkit
 */
 ?>
-               <div class="spring-footer">
-                   
+<div class="spring-footer">
+                  
+                <div class="container-fluid"> 
                    <div class="row">
                        
-                      <div class="col">
+                      <div class="col-sm-12 col-md-8">
                    
-                   <a href="https://www.germanna.edu/" target="_blank"><img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/germanna-logo-white.svg" class="custom-logo" alt="germanna logo" width="264" height="79"></a>
+                   <a href="https://www.germanna.edu/" target="_blank"><img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/germanna-logo-white.svg" class="custom-logo mb-3 mb-md-0 mx-auto" alt="germanna logo" width="264" height="79"></a>
                     
                       
                       </div>
                       
-                      <div class="col">
+            <div class="col-sm-12 col-md-4">
 
                   <?php if ( have_rows( 'footer_links', 'options' ) ) : ?>
 
-                    <ul class="list-inline text-right">
+                    <ul class="list-inline text-sm-center text-md-right">
                       <?php while ( have_rows( 'footer_links', 'options' ) ) : the_row(); ?>
                         <li class="list-inline-item"><a href="<?php the_sub_field( 'link_url', 'options' );?>">
                           <?php the_sub_field( 'link_text', 'options' ); ?>  
@@ -35,13 +36,14 @@
 
                   <?php endif; ?>
 
-               <p class="text-right">&copy; <?php echo date('Y'); ?> 
+               <p class="text-sm-center text-md-right">&copy; <?php echo date('Y'); ?> 
                 <?php _e('Germanna Community College', 'gcc-starter-kit') ?>
                </p>
               
-              </div>
+            </div>
                    </div>
-                   
+          
+          </div>         
               </div>
 <?php wp_footer(); ?>
 </body>
