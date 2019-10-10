28b4bd6f068d990d59d59995dc5daa3229b25b3e
Matt Hummel <marketing@users.noreply.github.com>
2019-10-10 11:52:43 -0400

    Update
    Removed enqueue script for google fonts since they are in sass.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -4,11 +4,9 @@
  */
 function gcc_starter_kit_scripts() {
 
-	wp_enqueue_style( 'gcc-landing-style', get_stylesheet_directory_uri() . '/assets/css/style.min.css');
+	wp_enqueue_style( 'gcc-landing-style', get_stylesheet_directory_uri() . '/assets/css/styleV2.min.css', array(), '0.1.6', 'all');
 
-	//wp_enqueue_style( 'gcc-landing-font', '//fonts.googleapis.com/css?family=Oswald&display=swap');
-	
-   wp_enqueue_script( 'gcc-landing-scripts', get_stylesheet_directory_uri() .  '/assets/js/main.mins.js', array(), '20151215', true );
+   wp_enqueue_script( 'gcc-landing-scripts', get_stylesheet_directory_uri() .  '/assets/js/main.min.js', array(), '20151215', true );
 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
